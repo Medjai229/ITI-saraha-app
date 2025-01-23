f73b60d7c759571119bf70bdba4b01d33cd7e424
Malik Hussein <malikhussein229@gmail.com>
2025-01-23 21:57:50 +0200

    Added the validation middleware in the routes

--- src/modules/auth/auth.controller.js
+++ src/modules/auth/auth.controller.js
@@ -1,10 +1,15 @@
 import { Router } from 'express';
 import Auth from './service/auth.service.js';
+import { validation } from '../../utils/validation.js';
+import {
+  signInValidationSchema,
+  signUpValidationSchema,
+} from './auth.validation.js';
 
 const authRouter = Router();
 
-authRouter.post('/register', Auth.register);
-authRouter.post('/login', Auth.login);
+authRouter.post('/register', validation(signUpValidationSchema), Auth.register);
+authRouter.post('/login', validation(signInValidationSchema), Auth.login);
 authRouter.get('/verify/:token', Auth.verify);
 
 export default authRouter;

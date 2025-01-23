a368256570b97d3423cf287a0000d352e63eefaa
Malik Hussein <malikhussein229@gmail.com>
2025-01-23 21:58:59 +0200

    Added the sign up and sign in joi schema

---
+++ src/modules/auth/auth.validation.js
@@ -0,0 +1,25 @@
+import joi from 'joi';
+
+export const signUpValidationSchema = joi
+  .object({
+    name: joi.string().min(2).max(15).required(),
+    email: joi
+      .string()
+      .email({ minDomainSegments: 2, tlds: ['com', 'net'] })
+      .required(),
+    password: joi.string().required(),
+    confirmPassword: joi.string().valid(joi.ref('password')).required(),
+    phone: joi.string().required(),
+    gender: joi.string(),
+  })
+  .options({ allowUnknown: false });
+
+export const signInValidationSchema = joi
+  .object({
+    email: joi
+      .string()
+      .email({ minDomainSegments: 2, tlds: ['com', 'net'] })
+      .required(),
+    password: joi.string().required(),
+  })
+  .options({ allowUnknown: false });

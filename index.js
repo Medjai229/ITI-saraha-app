a0b1e14781242287889362816afe79e9cdecfda3
Malik Hussein <malikhussein229@gmail.com>
2025-02-13 18:41:13 +0200

    Added cors

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ import * as dotenv from 'dotenv';
 import e from 'express';
 import bootstrap from './src/app.controller.js';
 import connectDB from './src/db/connection.js';
+import cors from 'cors';
 dotenv.config({});
 connectDB();
 
 const app = e();
+app.use(
+  cors({
+    origin: '*',
+  })
+);
 
 bootstrap(app, e);
 
